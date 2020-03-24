588c83f39b64025a0a68ad6a193041dd64666b2b
P M <paula.melli2711@gmail.com>
2020-03-24 04:11:32 -0300

    Mejora en legibilidad
    Se quitaron variables globales y cambiaron variables por constantes. Se desarrollo funciones con nombres mas claros. Se cambio el contenido de las funciones de modo que fueran reutilizables más fácilmente.

--- household-data.js
+++ household-data.js
@@ -6,6 +6,36 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+
+function hideFirstForm (){
+    document.getElementById('number-of-members').style.display = 'none';
+}
+
+function showSecondForm () {
+    document.getElementById('reveal-after').style.display = 'inline';
+}
+
+function createARowForEachMember () {
+    const numberOfFamilyMembers = Number(document.getElementById('family-members').value);
+    const $forRows = document.getElementById('add-rows');
+
+    for (let i = 0; i < numberOfFamilyMembers; i++) {
+        let index = i + 1;
+        const label = document.createElement('label');
+        label.textContent = 'Member ' + index; 
+        $forRows.appendChild(label);
+
+        const li = document.createElement('li');
+        const input = document.createElement('input');
+        input.type = 'number';
+        input.className = 'age';
+        li.appendChild(input);
+        $forRows.appendChild(li);
+    }
+
+}
+
+
 const $nextStepButton = document.getElementById('next-step');
 
 const $goBackButton = document.getElementById('go-back');
@@ -13,26 +43,9 @@ const $goBackButton = document.getElementById('go-back');
 const $calculateButton = document.getElementById('calculate');
 
 $nextStepButton.onclick = function () {
-    document.getElementById('number-of-members').style.display = 'none';
-    document.getElementById('reveal-after').style.display = 'inline';
-
-    let n = Number(document.getElementById('family-members').value);
-    const $forRows = document.getElementById('add-rows');
-
-    for (let i = 0; i < n; i++) {
-        let x = i + 1;
-        let label = document.createElement('label');
-        let labelText = document.createTextNode('Member ' + x);
-        label.appendChild(labelText);
-        $forRows.appendChild(label);
-        let li = document.createElement('li');
-        let input = document.createElement('input');
-        input.setAttribute("type","number");
-        input.setAttribute("class","age");
-        li.appendChild(input);
-        $forRows.appendChild(li);
-    }
-
+    hideFirstForm();
+    showSecondForm();
+    createARowForEachMember();
     return false;
 }
 
@@ -40,54 +53,59 @@ $goBackButton.onclick = function() {
     location.reload();
 }
 
-let array = [];
-let oldest;
-let youngest;
-let average;
 
-function agesIntoArray() {
-for (let i = 0; i < (document.getElementsByClassName('age').length); i++) {
-    let item = Number(document.getElementsByClassName('age').item(i).value)
+function HTMLCollectionIntoArray(HTMLCollection) {
+    let array = [];
+for (let i = 0; i < (HTMLCollection.length); i++) {
+    let item = Number(HTMLCollection.item(i).value)
     array.push(item);
 }
+
+    return array;
 }
 
-function getOldestMember() {
-    oldest = Math.max.apply(Math, array);
-    
+function getOldestMember(array) {
+    const oldest = Math.max.apply(Math, array);
     return oldest;
 }
 
-function getYoungestMember() {
-    youngest = Math.min.apply(Math, array);
-
+function getYoungestMember(array) {
+    const youngest = Math.min.apply(Math, array);
     return youngest;
 }
 
-function getAgeAverage() {
+function getAgeAverage(array) {
     let arraySum = 0;
     for (let i = 0; i < array.length; i++) {
         let arrayItem = Number(array[i]);
         arraySum += arrayItem;
     }
 
-    average = arraySum / array.length
+    average = (arraySum / array.length).toFixed(2);
 
     return average;
 
 }
 
+function showResults(oldest, youngest, average) {
+    document.getElementById('oldest-member').value = oldest;
+    document.getElementById('youngest-member').value = youngest;
+    document.getElementById('age-average').value = average;
+}
+
+function clearDataForNewCalculation (array) {
+    array = [];
+}
+
 $calculateButton.onclick = function () {
-agesIntoArray();
-getOldestMember();
-getYoungestMember();
-getAgeAverage();
+    
+    let array = HTMLCollectionIntoArray(document.getElementsByClassName('age'));
+    const oldest = getOldestMember(array);
+    const youngest = getYoungestMember(array);
+    const average = getAgeAverage(array);
+    showResults(oldest, youngest, average);
 
-document.getElementById('oldest-member').value = oldest;
-document.getElementById('youngest-member').value = youngest;
-document.getElementById('age-average').value = average;
+    clearDataForNewCalculation(array);
 
-array = [];
-
-return false;
+    return false;
 }
